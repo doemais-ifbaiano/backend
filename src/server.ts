c9badd34063ba1c5df82a2bac63fcbad572bc796
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-17 22:57:49 -0300

    feat: add authRoutes and userRoutes on server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from 'express';
+import userRoutes from './routes/userRoutes'; 
+import authRoutes from './routes/authRoutes';
 
 const PORT = 3000;
 
 const app = express();
+app.use(express.json());
 
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 app.use('/', (req:Request, res:Response) => {
     res.send('Backend Doe Mais')
 });
