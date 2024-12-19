96405c890f399a1a5af610759ad167d25f13f17e
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-18 23:38:38 -0300

    feat: create functions to assist with authentication via Google

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { PrismaClient } from '@prisma/client';
 
+
 const prisma = new PrismaClient();
 
 interface LoginInput {
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -26,5 +26,37 @@ export default class UserService {
         return await prisma.usuarios.findUnique({
             where: { email }
         });
+    } 
+
+    static async findUserById(id : any) {
+        return await prisma.usuarios.findUnique({
+            where: { id }
+        });
+    }
+
+    static async generateRandomPassword() {
+        const randomPassword = Math.random().toString(36).slice(-6);
+        
+        const hashedPassword = await bcrypt.hash(randomPassword, 10);
+        return hashedPassword;
+    }
+    
+    static async findOrCreateUser(profile : any) {
+        let user = await prisma.usuarios.findFirst({
+            where: { google_id: profile.id }
+        })
+
+        if (!user) {
+            const password = await this.generateRandomPassword();
+            user = await prisma.usuarios.create({
+                data: {
+                    nome_usuario: profile.displayName,
+                    senha: password,
+                    email: profile.emails[0].value,
+                    google_id: profile.id, 
+                },
+            });
+        }
+        return user;
     }
 }
