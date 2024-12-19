c0f1efbb907b9eb49aa7cbc3bbd30fef69d2d182
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-18 23:39:44 -0300

    feat: add google authencticate and callback

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -6,4 +6,8 @@ const router = Router();
 
 router.post('/login', AuthController.login);
 
+router.get('/google', AuthController.googleAuthenticate);
+
+router.get('/google/callback', AuthController.googleCallback);
+
 export default router;
