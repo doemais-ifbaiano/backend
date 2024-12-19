535d1e26d69e01e9b65b3b4cd9acc73a580ae575
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-18 23:33:32 -0300

    feat: add google authenticate and callback

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,9 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import AuthService from "../services/authService";
+import passport from "passport";
 
 export default class AuthController {
+
     static async login(req: Request, res: Response) {
         const { username, password } = req.body;
 
@@ -11,5 +13,15 @@ export default class AuthController {
         } catch (error : any) {
             res.status(400).json({ error: error.message });
         }
+    }
+    
+    static googleAuthenticate(req: Request, res: Response) {
+        passport.authenticate('google', { scope: ['profile', 'email'] })(req, res);
+    }
+
+    static googleCallback(req: Request, res: Response) {
+        passport.authenticate('google', { failureRedirect: '/' })(req, res, () => {
+            res.redirect('/dashboard');
+        });
     }
 }
