e8d374ed6a344901a91188f6725f7eaa768fa40e
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2025-01-08 19:23:42 -0300

    refactor: melhora as mensagens de erro de acordo com a task

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,16 +1,13 @@
 import * as admin from "firebase-admin";
 
-
 export default class AuthService {
-    
-    /*No caso o firebase com auth já consegue gerar o token JWT pelo frontend facilmente, assim, não se faz 
-    necessário a criação do gerador de token e nem de middleware para validação do token*/
     static async verifyToken(idToken: string) {
         try {
             const decodedToken = await admin.auth().verifyIdToken(idToken);
-            console.log("Token válido")
+            console.log("Token válido.");
             return decodedToken;
-        } catch (error) {
+        } catch (error : any) {
+            console.error(`Erro na validação do token: ${error.message}`);
             throw new Error("Token inválido");
         }
     }
